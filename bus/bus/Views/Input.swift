15eafd20cf6fa2e2284a6b483f595491bb7c63b8
Jiu Choe <jiuchoe@ucdavis.edu>
2021-02-19 20:52:18 -0800

    Added input field

---
+++ bus/bus/Views/Input.swift
@@ -0,0 +1,33 @@
+//
+//  Input.swift
+//  bus
+//
+//  Created by Jiu Choe on 2/19/21.
+//
+
+import SwiftUI
+
+let lightGreyColor = Color(red: 239.0/255.0, green: 243.0/255.0, blue: 244.0/255.0, opacity: 1.0)
+
+struct Input: View {
+    //State property that holds the user's onput
+    @State var userInput = ""
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Name")
+                .font(.headline)
+            TextField("Enter destination...", text: $userInput)
+                .padding()
+                .background(lightGreyColor)
+                .cornerRadius(5.0)
+                .padding()
+        }
+    }
+}
+
+struct Input_Previews: PreviewProvider {
+    static var previews: some View {
+        Input()
+    }
+}
--- bus/bus/Views/Menu.swift
+++ bus/bus/Views/Menu.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct Menu: View {
     var body: some View {
         TabView(selection: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Selection@*/.constant(1)/*@END_MENU_TOKEN@*/) {
-            Text("Tab Content 1").tabItem { /*@START_MENU_TOKEN@*/Text("Tab Label 1")/*@END_MENU_TOKEN@*/ }.tag(1)
+            Input().tabItem { /*@START_MENU_TOKEN@*/Text("Tab Label 1")/*@END_MENU_TOKEN@*/ }.tag(1)
             Settings().tabItem {
                 // Insert gear picture
                 Text("Settings") }.tag(2)
