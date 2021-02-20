772579f1337985f17397e3e4dda8e6a1122fdf71
LucienLuc <lucienluc@umail.ucsb.edu>
2021-02-19 18:04:07 -0800

    Init commit

---
+++ bus/bus/busApp.swift
@@ -0,0 +1,17 @@
+//
+//  busApp.swift
+//  bus
+//
+//  Created by Lucien Luc on 2/19/21.
+//
+
+import SwiftUI
+
+@main
+struct busApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
