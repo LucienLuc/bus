6bf4100f09bf973eab79a3972f7e10158e23fbf9
LucienLuc <lucienluc@umail.ucsb.edu>
2021-02-20 00:14:12 -0800

    Revert "Some design changes"
    This reverts commit bfec0f173f8e0f88cb0b2843af449ac768bb3843.

--- bus/bus/Views/Settings.swift
+++ bus/bus/Views/Settings.swift
@@ -9,48 +9,37 @@ import SwiftUI
 
 struct Settings: View {
     
-    @State private var notif_radius = 50.0
+    @State private var speed = 50.0
     @State private var isEditing = false
 
     var body: some View {
         VStack (){
-            ZStack() {
             Text("Settings")
-                .font(.system(size: 24, weight: .bold, design: .monospaced))
+                .font(.title)
                 .fontWeight(.medium)
-                .foregroundColor(.gray)
                 .padding(.all)
-                .zIndex(/*@START_MENU_TOKEN@*/1.0/*@END_MENU_TOKEN@*/)
-                Capsule()
-                    .frame(width: 200, height: 50)
-                    .foregroundColor(Color(red: 20.0/255 , green: 56.0/255, blue: 139.0/255))
-            }
             Label("Notification Radius", systemImage: /*@START_MENU_TOKEN@*/"42.circle"/*@END_MENU_TOKEN@*/)
-                .alignmentGuide(HorizontalAlignment.leading) { _ in 1 }
+                .alignmentGuide(HorizontalAlignment.leading) { _ in  -500 }
             HStack() {
                 Text("0")
                 Slider(
-                    value: $notif_radius,
+                    value: $speed,
                     in: 0...100,
                     step: 5,
                     onEditingChanged: { editing in
                         isEditing = editing
                     }
                 ) {
-                    Text("notif_radius")
+                    Text("Speed")
                 }
                 Text("100")
             }
                 .padding(.all)
-            HStack {
-                Text("\(notif_radius, specifier: "%.0f")")
+            Text("\(speed)")
                     .foregroundColor(isEditing ? .red : .blue)
-                Text("miles")
-            }
         }
         .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topLeading)
     }
-    
 }
 
 struct Settings_Previews: PreviewProvider {
