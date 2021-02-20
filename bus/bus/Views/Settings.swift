067b8bf941f5d62ca2510c46de1efd6144698230
LucienLuc <lucienluc@umail.ucsb.edu>
2021-02-19 19:23:51 -0800

    Menu

---
+++ bus/bus/Views/Settings.swift
@@ -0,0 +1,20 @@
+//
+//  Settings.swift
+//  bus
+//
+//  Created by Lucien Luc on 2/19/21.
+//
+
+import SwiftUI
+
+struct Settings: View {
+    var body: some View {
+        Slider(value: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant(10)/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Settings_Previews: PreviewProvider {
+    static var previews: some View {
+        Settings()
+    }
+}
