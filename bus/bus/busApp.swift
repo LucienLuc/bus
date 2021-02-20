8f6aa19060447e008d9af31c0a1fec58820e5f4a
LucienLuc <lucienluc@umail.ucsb.edu>
2021-02-20 00:26:26 -0800

    Revert "Revert "made an oopsie withe the api key""
    This reverts commit 6e4d15cb1d7e964a0735ae3ecffb331da4b32747.

--- bus/bus/busApp.swift
+++ bus/bus/busApp.swift
@@ -9,10 +9,13 @@ import SwiftUI
 import GoogleMaps
 import GooglePlaces
 
+let GOOGLE_CLOUD_API = ProcessInfo.processInfo.environment["GOOGLE-CLOUD-API"]
+
+
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        GMSServices.provideAPIKey("AIzaSyB2Et4BCEUILyxCTPd6sXZixlz7RjXkT70")
-        GMSPlacesClient.provideAPIKey("AIzaSyB2Et4BCEUILyxCTPd6sXZixlz7RjXkT70")
+        GMSServices.provideAPIKey(GOOGLE_CLOUD_API!)
+        GMSPlacesClient.provideAPIKey(GOOGLE_CLOUD_API!)
         return true
     }
 }
